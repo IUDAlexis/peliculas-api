bc4d966938e00d71ee184c21fb3fc169dc989700
ALEXISCQ <e.alexisceballos@gmail.com>
2025-08-27 20:51:15 -0500

    ajuste

--- src/database.js
+++ src/database.js
@@ -2,15 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-<<<<<<< HEAD
-    await mongoose.connect("mongodb://localhost:27017/peliculas_db", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ Conectado a MongoDB");
-  } catch (err) {
-    console.error("❌ Error en la conexión:", err);
-=======
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -19,7 +10,6 @@ const connectDB = async () => {
   } catch (err) {
     console.error("❌ Error de conexión:", err.message);
     process.exit(1);
->>>>>>> e0300e1 (Add .gitignore to exclude node_modules directory)
   }
 };
 
